b6694766b82842dadad6012933f4ed748812ba72
Andrey Ryabtsev <ryab@yandex.ru>
2023-07-19 19:50:51 +0300

    added eslint and prettier

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-}) 
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
